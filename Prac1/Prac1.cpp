f41f91b5e1682bfbada22725d475784b9ba31074
Scott Klein <scotty.klei@gmail.com>
2020-03-03 18:55:34 +1100

    Set Up the practical

--- Prac1/Prac1.cpp
+++ Prac1/Prac1.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+template <class Type>
+void funcExp(Type list[], int size);
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n";
+}
+
+
+
+
+template <class Type>
+void funcExp(Type list[], int size) 
+{ 
+	Type x = list[0]; 
+
+	Type y = list[size - 1]; 
+	for (int j = 1; j < size; j++) 
+	{ 
+		if (x < list[j])
+			x = list[j];
+		if (y > list[size - 1 - j])
+			y = list[size - 1 - j];
+	}
+	cout << x << endl; cout << y << endl; 
 }
