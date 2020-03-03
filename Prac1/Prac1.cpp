063d9fe485daee03883ff2abb110b2bf64d36c87
Scott Klein <scotty.klei@gmail.com>
2020-03-03 20:33:06 +1100

    Completed practical 1.1 with comments

--- Prac1/Prac1.cpp
+++ Prac1/Prac1.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 template <class Type>
 void funcExp(Type list[], int size);
 
+int list[10] = { 5,3,2,10,4,19,45,13,61,11 }; 
+string strList[] = { "One", "Hello", "Four", "Three", "How", "Six" };
+
+
 int main()
 {
-	std::cout << "Hello World!\n";
+	cout << "A:\n";
+	funcExp(list, 10);
+
+	cout << "B:\n";
+	funcExp(strList, 6);
+
+
+	if ("C" > "A")
+	{
+		cout << "True";
+	}
+	else
+	{
+		cout << "False";
+	}
 }
 
 
 
-
+//For numbers prints the largest number, then smallest
+// For strings prints the latest word in dictionary order, then the earliest.
 template <class Type>
 void funcExp(Type list[], int size) 
 { 
@@ -19,10 +40,14 @@ void funcExp(Type list[], int size)
 	Type y = list[size - 1]; 
 	for (int j = 1; j < size; j++) 
 	{ 
+		//Finds the largest number starting from the front of the list.
 		if (x < list[j])
 			x = list[j];
+
+		//Finds the smallest number starting from the back of the list.
 		if (y > list[size - 1 - j])
 			y = list[size - 1 - j];
 	}
-	cout << x << endl; cout << y << endl; 
+	cout << x << endl; 
+	cout << y << endl; 
 }
